f72012e9700fbb0cbef856f945409c9768489c2e
Shuvo <suvo10.info@gmail.com>
2025-08-27 00:27:24 +0600

    hdhydh

---
+++ oop1.py
@@ -0,0 +1,29 @@
+class Book:
+    def__init__(self,bookId:int,title:str,author:str):
+         self.bookId=bookId 
+         self.title=title
+         self.author=autho
+         self.isAvailable=True 
+
+
+
+class User:
+    def__init__(self,userId:int,name=str):
+        self.userId=userId
+        self.name=name 
+        self.borrowedbooks=[]
+
+
+
+
+
+
+
+class  library:
+    def__init__(self) :
+     self.books={}
+      self.users={}
+    
+        
+    
+
