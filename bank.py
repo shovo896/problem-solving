ddd06007d206edfe179e92eabd706c81a78b3c44
Shuvo <suvo10.info@gmail.com>
2025-08-30 23:54:12 +0600

    shshhs

--- bank.py
+++ bank.py
@@ -7,3 +7,15 @@ class BankAccount:
         if amount>0:
             self.balance+=amount
             print(f"{amount} deposoted.New balance:{self.balance}")
+        else:
+            print("Deposit number must be positive")
+
+    def withdraw(self,amount):
+        if amount <= self.balance:
+            print(f"{amount}withdraw.New balance:{self.balance}")
+        else:
+            print("Insufficient balance")
+    def __str__(self):
+        return f"Account {self.account_number}-Holder:{self.holder_name}-Balance(self.balance)"
+    
+    
