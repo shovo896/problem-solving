a1f978b5e352626e63e01fb6a15557d6fb3569b7
Shuvo <suvo10.info@gmail.com>
2025-10-07 12:17:46 +0600

    ccnnc

--- dutuu.c
+++ dutuu.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-    printf("Hello World");
-    return 0;
-}
+    int arr[3]={1,2,3};
+    int *p=arr;
+    for (int i=0;i<3;i++)
+    {
+        printf("arr[%d]=%d\n (via pointer) *(p+%d)=%d\n",i,arr[i],i,*(p+i));
+    }
