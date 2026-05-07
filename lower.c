b263296dde2eddc445e077610ec21abaadcd66e9
Shuvo <suvo10.info@gmail.com>
2026-05-07 22:23:13 +0600

    j;ol

--- lower.c
+++ lower.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
-int main(){
-       char c;
-       scanf("%c",&c);
-       char ans;
-       ans = c - ('a'+1);
-       printf("%c",ans);
-       return 0;
+#include <ctype.h>
+#include <stdio.h>
 
+int main() {
+       char c;
+
+       if (scanf("%c", &c) != 1) {
+              return 1;
+       }
+
+       printf("%c", tolower((unsigned char)c));
+       return 0;
 }
