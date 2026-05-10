f4533b1aa5af3aa8871cfa555ff60a731f441246
Shuvo <suvo10.info@gmail.com>
2026-05-10 22:29:38 +0600

    juhoil

--- weighted.c
+++ weighted.c
@@ -1,10 +1,9 @@
 #include<stdio.h> 
 int main(){
-       int w1,q1,w2,q2;
-       scanf("%d%d%d%d",&w1,&q1,&w2,&q2);
-       float ans= (float)(w1*q1+w2*q2)/(q2+q1);
-       printf("%.2f",ans);
+       int n;
+       scanf("%d",&n);
+       int ans = (n + 1) / 2;
+       printf("%d",ans);
        return 0;
-
 
 }
