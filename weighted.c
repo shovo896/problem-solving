4067287c4a5bcb834418954c8bb0f02173b67611
Shuvo <suvo10.info@gmail.com>
2026-05-10 22:54:34 +0600

    kdmk

--- weighted.c
+++ weighted.c
@@ -1,16 +1,10 @@
 #include<stdio.h> 
 int main(){
-       int n,m;
-       scanf("%d%d",&n,&m);
-       int sum=0;
-       for(int i=0;i<n;i++){
-              int w,v;
-              scanf("%d%d",&w,&v);
-              if(w<=m){
-                     sum+=v;
-              }
-       }
-       printf("%d",sum);
+       int x, y;
+       scanf("%d%d",&x,&y);
+       int perDay = x / 20;
+       int extraSalary=y*perDay*1.5;
+       int totalSalary=x+extraSalary;
+       printf("%d",totalSalary);
        return 0;
-
 }
