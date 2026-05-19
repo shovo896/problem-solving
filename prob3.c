14c85da8b308abce30427c28e486608b191cb240
Shuvo <suvo10.info@gmail.com>
2026-05-19 23:33:52 +0600

    wuu

---
+++ prob3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+       int x = 11;
+       scanf("%d", &x);
+       if (x>=10 && x<= 20 || x == 10){
+              printf("Yes\n");
+       }
+       else{
+              printf("No\n");
+       }
+       return 0;
+}
