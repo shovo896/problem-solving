dcfc5c948a8eba0ea4c4718a9814a9ef97527d91
Shuvo <suvo10.info@gmail.com>
2026-05-19 23:27:37 +0600

    euwhu

---
+++ condi2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+       int x=10;
+       scanf("%d",&x);
+       if (x>= 10 && x <=20){
+              printf("Yes\n");
+       }
+       else{
+              printf("No\n");
+       }
+}
