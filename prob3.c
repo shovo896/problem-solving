562c9fcb3fb6397d5797a5daea02e54706bdb0ed
Shuvo <suvo10.info@gmail.com>
2026-05-19 23:35:35 +0600

    desyg

--- prob3.c
+++ prob3.c
@@ -2,7 +2,7 @@
 int main() {
        int x = 11;
        scanf("%d", &x);
-       if (x>=10 && x<= 20 || x == 10){
+       if ((x >= 10 && x <= 20) || x == 10){
               printf("Yes\n");
        }
        else{
