25a38425a5333d41e2412f81bd0f2009e826d3fc
Shuvo <suvo10.info@gmail.com>
2026-05-24 11:31:12 +0600

    ng

--- conditional.c
+++ conditional.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
 int main (){
-    int x ;
+    int x ,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+
+
+    if (x>y){
+        if (y-x <= 2 )
+        printf("Yes\n");
+        else
+        printf("No\n");
     
-}
+    }
+    if (y>x){
+        if (x-y <= 2 )
+        printf("Yes\n");
+        else
+        printf("No\n");
+    
+    }
+    if (x==y){
+        printf("No\n");
+    }
+    return 0;
+}   
+
