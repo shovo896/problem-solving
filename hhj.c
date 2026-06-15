9bb29954cdd28096246794569a0c662c2342fe21
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-15 23:00:45 +0600

    hbjbh

--- hhj.c
+++ hhj.c
@@ -1,21 +1,16 @@
-#include<stdio.h> 
-int main(){
+#include <stdio.h>
+
+int main(void) {
     int x;
-    scanf("%d",&x);
-    for (int i=1 ;i*i<=x;i++){
-        if (x%i==0){
-            if(i*i==x){
-                printf("%d ",i);
-            }
-            else{
-            printf("%d ",i);
-            if (i*i!=x){
-                //exact handle perfect square
-                
-                printf("%d ",x/i);
+    if (scanf("%d", &x) != 1) return 0;
+    for (int i = 1; i * i <= x; i++) {
+        if (x % i == 0) {
+            if (i * i == x) {
+                printf("%d ", i);
+            } else {
+                printf("%d %d ", i, x / i);
             }
         }
     }
     return 0;
-
 }
