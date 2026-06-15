1d5b7beb8a62ddea75892f098a6b3a3c9ec264f9
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-15 22:44:32 +0600

    bhbbjh

--- bjh.c
+++ bjh.c
@@ -4,6 +4,20 @@ int main(){
     scanf("%d",&x);
 
 
+    int p=x,n=0;
+    while(x!=0){
+        int last=x%10;
+        n=n*10+last;
+        x/=10;
+    }
+    if (p==n){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+
+
 
 
     return 0 ;
