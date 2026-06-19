46b8e71ae59e2b8bcfccf02394b5cf177a289c67
Shuvo <suvo10.info@gmail.com>
2026-06-19 22:15:31 +0600

    dfa

--- see7.c
+++ see7.c
@@ -1,19 +1,11 @@
 #include<stdio.h> 
 int main(){
-       int n ;
+       int n;
        scanf("%d",&n);
-       int arr[n];
-
-       for (int i=0;i<n;++i){
+       for (int i = 0; i < n; ++i){
               scanf("%d",&arr[i]);
        }
-       int odd = 0, even = 0;
-       for (int i = 0; i < n;i+=2){
-              odd += arr[i];
+       for (int i = n - 1; i >= 0;--i){
+              printf("%d ", arr[i]);
        }
-       for (int i = 1; i < n;i+=2){
-              even += arr[i];
-       }
-       printf("Sum of odd-indexed elements: %d\n", odd);
-       printf("Sum of even-indexed elements: %d\n", even);
 }
