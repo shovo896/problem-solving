4657f8e657115592b5eda651c7ee8cb335fe4002
Shuvo <suvo10.info@gmail.com>
2026-06-19 22:49:52 +0600

    edsfw

--- ehas.c
+++ ehas.c
@@ -3,5 +3,16 @@ int main(){
        int n; 
        scanf("%d",&n);
        int arr[n];
-       
+       int left = 0, right = n - 1;
+       while(left < right){
+
+              int temp = arr[left];
+              arr[left] = arr[right];
+              arr[right] = temp;
+              left += 1;
+              right -= 1;
+       }
+       for (int i = 0; i < n; ++i){
+              printf("%d ", arr[i]);
+       }
 }
---
+++ fikvkji.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+       int t;
+       scanf("%d",&t);
+       for (int tc = 0;tc<t;++tc){
+
+       int n;
+       scanf("%d",&n);
+       int arr[n];
+       for (int i = 0; i < n; ++i){
+              scanf("%d",&arr[i]);
+       }
+}// bujlam na bal 
+}
