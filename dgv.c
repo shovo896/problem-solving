d862853468f7bbdba36a5011de4960ae04d16ba3
Shuvo <suvo10.info@gmail.com>
2026-06-25 22:08:35 +0600

    fbs

---
+++ dgv.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+       int brr[4][3];
+       for (int r = 0; r < 4;++r){
+              for (int c = 0; c < 3;++c){
+                     
+
+              }
+       }
+       return 0;
+}
