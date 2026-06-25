3dac63cfad7e37650f88ea1bb8cf5b016acc9297
Shuvo <suvo10.info@gmail.com>
2026-06-25 22:14:42 +0600

    dsiujsd

--- dgv.c
+++ dgv.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
 int main(){
-       int brr[4][3];
-       for (int r = 0; r < 4;++r){
-              for (int c = 0; c < 3;++c){
-                     
+       int crr[2][3] = {{{1, 2, 3}, {4, 5, 6}},
+                        {{1, 2, 3}, {4, 5, 6}}};
 
-              }
-       }
-       return 0;
+       printf("%d\n", crr[1][2]); // This will print the element at row 1, column 2
 }
