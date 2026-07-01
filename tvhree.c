5d26153b6a5a1b4390aadfc07acf221b91365cac
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-07-02 01:05:02 +0600

    chnb m

--- tvhree.c
+++ tvhree.c
@@ -1,36 +1,36 @@
-#include<stdio.h> 
-int main(){
-    int t ;
-    scanf("%d",&t);
-    while(t--){
-        int n ;
-        scanf("%d",&n);
-        int a[n];
-        scanf("%d",&n);
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
 
         int a[n];
-        int freq[n+1];
+        int freq[n + 1];
 
-        for (int i=0;i<n;i++){
-            freq[i]=0;
+        for (int i = 0; i <= n; i++) {
+            freq[i] = 0;
         }
-        for (int i=0;i<n;i++){
-            scanf("%d",&a[i]);
-            freq[a[i]]++;
 
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+            freq[a[i]]++;
         }
 
         int ans = -1;
 
-        // jeta tin bar asche seta khujo 
-        for (int i=0;i<n ;i++){
-            if (freq[a[i]] == 3){
+        for (int i = 0; i < n; i++) {
+            if (freq[a[i]] == 3) {
                 ans = a[i];
                 break;
             }
         }
-        printf("%d\n",ans);
-    }
-    return 0;   
-        }
 
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
