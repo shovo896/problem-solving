76fba7fb4b8da405cfe0dc30ce5b904d9b1a386a
Shuvo <suvo10.info@gmail.com>
2026-07-05 17:46:27 +0600

    fbdfs

--- edga.c
+++ edga.c
@@ -7,6 +7,19 @@ int main(){
        for (int i = 0; i < n; i++) {
               for (int j = 0; j < m; j++) {
                      scanf("%d", &arr[i][j]);
+
+
+              }
+       }
+       for (int i = 0; i < n;++i){
+              int l = 0, r = m - 1;
+              while (l<r){
+                     int temp = arr[i][l];
+                     arr[i][l] = arr[i][r];
+                     arr[i][r] = temp;
+                     l++;
+                     r--;
+
               }
        }
 }
