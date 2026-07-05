62f8ed0d8eaf715a925f4613edcee1f273712501
Shuvo <suvo10.info@gmail.com>
2026-07-05 20:22:15 +0600

    46ywaq

--- fah.c
+++ fah.c
@@ -1,4 +1,18 @@
 #include<stdio.h> 
 int main(){
-       
+       int h, w;
+       scanf("%d %d", &h, &w);
+       char arr[h][w];
+       for (int i = 0; i < h; ++i){
+              for (int j = 0; j < w; ++j){
+                     scanf(" %c", &arr[i][j]);
+              }
+              for (int i = 0; i < h; ++i)
+              {
+                     for (int j = 0; j < w; ++j)
+                     {
+                            printf(" %c", &arr[i][j]);
+                     }
+                     printf("\n");
+              }
 }
