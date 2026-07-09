d7e2213987c4afe5decac480b858089bb2f4190e
Shuvo <suvo10.info@gmail.com>
2026-07-09 21:45:29 +0600

    oiop;

--- f3e.c
+++ f3e.c
@@ -3,6 +3,16 @@ int main(){
        char str[20];
        scanf("%s", str);
        int n=strlen(str);
+       int i =0 ,j=n-1 ;
+       while(i<=j){
+              printf("%d-> %c\n", i, str[i]);
+              printf("%d-> %c\n", j, str[j]);
+              char temp=str[i];
+              str[i]=str[j];
+              str[j]=temp;
+              i++;
+              j--;
+       }
        printf("%s\n", str);
-       
+
 }
