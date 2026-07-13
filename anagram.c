a0516beafd753348921e399a8299b41cd725f06e
Shuvo <suvo10.info@gmail.com>
2026-07-14 02:36:55 +0600

    rhgse

--- anagram.c
+++ anagram.c
@@ -1,5 +1,17 @@
 #include<stdio.h> 
-#include<strings.h>
+#include<string.h>
+#include<ctype.h>
 int main(){
-       
+       char s[4];
+       scanf("%s",s);
+
+       for (int i = 0; i < 3; i++){
+              s[i] = tolower(s[i]);
+       }
+       if (strcmp(s,"yes") == 0){
+              printf("YES\n");
+       }
+       else{
+              printf("NO\n");
+       }
 }
