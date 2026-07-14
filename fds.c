65191b7b43d758277534185ba471d2e286850d18
Shuvo <suvo10.info@gmail.com>
2026-07-15 02:43:24 +0600

    fhxns

--- fds.c
+++ fds.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 int main(){
-       char s[11], t[11];
+       char s[17];
        scanf("%s",s);
-       scanf("%s",t);
-       printf("%s %s\n",s,t);
+       for (int i = 1; i < 16;i++){
+              if (s[i] != '0' ){
+                     printf("No\n");
+                     return 0;
+              }
+       }
+       printf("Yes\n");
        return 0;
 }
