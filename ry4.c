03df78a846609b9cbdf0d97d0f1dcefb8c587012
Shuvo <suvo10.info@gmail.com>
2026-07-23 22:29:35 +0600

    fgd

--- ry4.c
+++ ry4.c
@@ -1,5 +1,14 @@
 #include<stdio.h> 
 #include<string.h>
 int main(){
-       
+       int n;
+       scanf("%d",&n);
+       for (int a = 1; a <= n;a++){
+              for (int b = a;b <= n;b++){
+                     for (int c = b; c <= n; c++){
+                            printf("%d %d %d\n",a,b,c);
+                     }
+              }
+              printf("\n");
+       }
 }
