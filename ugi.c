d4ca0194729d3e45ecd80f557412e693da8983c3
Shuvo <suvo10.info@gmail.com>
2026-07-23 13:12:37 +0600

    fhxs

--- ugi.c
+++ ugi.c
@@ -1,5 +1,14 @@
 #include<stdio.h> 
 #include<string.h>
 int main(){
-       char 
+       char names[3][6];
+       char st[6];
+       scanf("%s",st);
+       printf("%s\n",st);
+       for (int i = 0; i < 3; i++){
+              scanf("%s",names[i]);
+       }
+       for (int i = 0; i < 3;i++){
+              printf("%s\n",names[i]);
+       }
 }
