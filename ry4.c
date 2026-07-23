1ded27e25f947ce42871c6af65834fff0fab84bc
Shuvo <suvo10.info@gmail.com>
2026-07-23 22:32:44 +0600

    rfds

--- ry4.c
+++ ry4.c
@@ -2,13 +2,18 @@
 #include<string.h>
 int main(){
        int n;
+       int ans = 0;
        scanf("%d",&n);
        for (int a = 1; a <= n;a++){
               for (int b = a;b <= n;b++){
                      for (int c = b; c <= n; c++){
                             printf("%d %d %d\n",a,b,c);
+                            if (a*a +b*b ==c*c){
+                                   ans = ans + 1;
+                            }
                      }
               }
-              printf("\n");
+              printf("%d\n",ans);
+              return 0;
        }
 }
