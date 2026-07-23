61745d0af4353466dbb55d9cf2720d5fecf3298c
Shuvo <suvo10.info@gmail.com>
2026-07-23 13:59:35 +0600

    resgd

--- hbjh.c
+++ hbjh.c
@@ -3,11 +3,20 @@
 int main(){
        char sr[3][4];
        for (int i = 0; i < 3;i++){
-              scanf("%s",sr[i]);
+              scanf("%c",sr[i]);
        }
        for (int i = 0; i < 3;i++){
-              if (sr[0][0] !='a' && sr[0][1]!='a' && sr[i][2]!='a'){
-                     printf("%s\n",sr[i]);
+              if (sr[i][0] !='a' && sr[i][1]!='a' && sr[i][2]!='a'){
+                     printf("%c\n",sr[i]);
+              }
+              if (sr[i][0] != 'b' && sr[i][1] != 'b' && sr[i][2] != 'b')
+                     {
+                            printf("%c\n", sr[i]);
+                     }
+              if (sr[i][0] != 'c' && sr[i][1] != 'c' && sr[i][2] != 'c')
+                     {
+                            printf("%c\n", sr[i]);
+                     }
               }
        }
        return 0;
