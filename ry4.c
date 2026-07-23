d1db35fa377170a89b41343d94e1792243de9eb5
Shuvo <suvo10.info@gmail.com>
2026-07-23 22:42:04 +0600

    yes

--- ry4.c
+++ ry4.c
@@ -1,19 +1,19 @@
 #include<stdio.h> 
 #include<string.h>
+#include<math.h>
 int main(){
        int n;
        int ans = 0;
        scanf("%d",&n);
        for (int a = 1; a <= n;a++){
               for (int b = a;b <= n;b++){
-                     for (int c = b; c <= n; c++){
-                            printf("%d %d %d\n",a,b,c);
-                            if (a*a +b*b ==c*c){
-                                   ans = ans + 1;
-                            }
+                     int sq_c = a * a + b * b;
+                     int c = sqrt(sq_c);
+                     if (c*c == sq_c){
+                            ans = ans + 1;
+                     }
                      }
               }
               printf("%d\n",ans);
               return 0;
        }
-}
