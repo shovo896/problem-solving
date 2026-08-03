3403a5afbb02820de0e8c83085ed69eaff850d86
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-03 22:30:53 +0600

    hnb j

--- vacation_int.c
+++ vacation_int.c
@@ -5,6 +5,14 @@ int main(){
     scanf("%d %d",&n,&d);
     char st[n][d+1];
     scanf("%s",st);
+
+    char st[d+1];
+    for(int j=0;j<d;j++){
+        for(int i=0;i<n;i++){
+            printf("%c",st[i][j]);
+        }
+        printf("\n")
+    }
     int cur = 0 ,mx=0;
     for(int i=0;i<d;i++){
         if(st[i]=='0'){
