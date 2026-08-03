0b3dc68166eb7d5ea548afa03b68047352aba7c4
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-03 22:26:46 +0600

    nv nj

--- vacation_int.c
+++ vacation_int.c
@@ -1,9 +1,9 @@
 #include<stdio.h> 
 #include<string.h>
 int main(){
-    int d ; 
-    scanf("%d",&d);
-    char st[d+1];
+    int n,d ; 
+    scanf("%d %d",&n,&d);
+    char st[n][d+1];
     scanf("%s",st);
     int cur = 0 ,mx=0;
     for(int i=0;i<d;i++){
