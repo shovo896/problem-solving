9164526d1029dce104f5926aa62334e840871a35
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-03 19:28:05 +0600

    jnjk

---
+++ grid3.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include<string.h>
+int main(){
+    char st[3][4];
+    
+    for (int i =0 ;i <3 ;i++){
+        scanf("%s",st[i]);
+    }
+    int sum = 3 *('a'+'b'+'c')+'?';
+    
+    
+}
