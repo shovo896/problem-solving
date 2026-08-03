d80f7c62486cdafecf9fff4c5ce75f0732f635cd
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-03 12:30:28 +0600

    gv

--- jcuhn.c
+++ jcuhn.c
@@ -2,12 +2,16 @@
 #include<string.h>
 int main(){
        char names[3][6];
+       char st[6];
+       scanf("%s",st);
+       printf("%s\n",st);
+       
        for (int i = 0; i < 3;i++){
               scanf("%s",names[i]);
        }
        for (int i = 0; i < 3;i++){
               printf("%s\n",names[i]);
        }
-       
+
 
     }
