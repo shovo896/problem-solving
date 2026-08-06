0f65699465abe7ecee3978f250f9245c037d5f38
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-06 14:18:32 +0600

    hbhj

---
+++ gxg.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main(){
+    int t ; 
+    scanf("%d",&t);
+    
+    while(t--){
+        int n ; 
+        scanf("%d",&n);
+        if (n==1){
+            printf("1\n");
+            continue;
+        }
+        for(int )
+
+    }
+}
