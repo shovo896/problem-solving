6026f8960eaa7ebc27f2e4e24389acc0e5a4b7ff
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 21:13:23 +0600

    chbhj

---
+++ fuc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(){
+
+
+}
+int main(){
+    int a = 5 , b=9 ;
+    printf(" a=%d , b=%d\n",a,b);
+    int c = a ; 
+    a = b ; 
+    b = c ;
+    printf(" a=%d , b=%d\n",a,b);
+    return 0 ;
+
+}
