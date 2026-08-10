b70038ce3bf690e5a4485e95af10ecdf494fa87a
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 17:25:58 +0600

    bikjio

--- paliondrome.c
+++ paliondrome.c
@@ -1,6 +1,19 @@
 #include<stdio.h> 
+
+int numRev(int x){
+    int y = 0;
+
+    while(x > 0){
+        int lastDigit = x % 10;
+        y = y * 10 + lastDigit;
+        x /= 10;
+    }
+
+    return y;
+}
+
 int isPalindrome(int x){
-    if (x==numRev(x))
+    if (x == numRev(x))
         return 1 ; 
     else
         return 0 ;
