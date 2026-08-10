ead8029a58163d1f3a187ee280e1a996decc6874
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 20:23:51 +0600

    juhho

---
+++ pointer_variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main(){
+    int a = 10 ; 
+    printf("%d\n",a);
+    printf("%p\n",(void*)&a);
+
+    int *b=&a ; 
+    printf("%d\n",*b);
+    printf("%p\n",(void*)b);
+    return 0 ;
+
+}
