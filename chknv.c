b93a5d1e6efb13aad970e2f16af25afa8c1c2f59
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 13:16:38 +0600

    gvk

--- chknv.c
+++ chknv.c
@@ -1,10 +1,20 @@
 #include<stdio.h> 
+
+
+void fun(int* arr) {
+
+    printf("%d\n", *(arr+1));
+}
 int main(){
     int arr[5] = {1,2,3,4,5} ; 
     printf("%d\n", arr[4]);
     printf("%d\n", arr[0]);
     printf("%d\n", arr[2]);
     printf("%d\n", arr[2] + 2);
+
+    // passing array 
+    fun(arr);
+
     return 0 ;
 
 
