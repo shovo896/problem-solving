1044d5985c0d019701c53cafd2474102207de19f
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 21:51:48 +0600

    foijoi

--- palinfrome.c
+++ palinfrome.c
@@ -1,22 +1,19 @@
-#include<stdio.h> 
+#include<stdio.h>
 
-void isPlaindrome(int* arr,int n){
-    for(int i=0 ;i <n/2;i++){
-        // i <----> n-i-1 ei duita soman 
-        if(arr[i]!=arr[n-i-1]){ 
-            // If any pair doesn't match, it's not a palindrome
-            return 0 ;
+int isPlaindrome(int *arr, int n){
+    for(int i = 0; i < n / 2; i++){
+        if(arr[i] != arr[n - i - 1]){
+            return 0;
         }
     }
-    return 1 ;// no mismatch found 
-
-    }
+    return 1;
 }
 
 int main(){
-    int n = 5 ; 
-    int arr[]={5,4,3,2,1}; 
-    printf("%d\n",isPlaindrome(arr,n));
-    return 0 ; 
-    
-    }
+    int n = 5;
+    int arr[] = {5, 4, 3, 2, 1};
+
+    printf("%d\n", isPlaindrome(arr, n));
+
+    return 0;
+}
