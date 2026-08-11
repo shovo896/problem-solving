11e6ee5266d9e2339131fbdf74b4544b74abcf46
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 21:48:44 +0600

    hbgkj

---
+++ palinfrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+
+void isPlaindrome(int* arr,int n){
+    for(int i=0 ;i <n/2;i++){
+
+    }
+}
+
+int main(){
+    int n = 5 ; 
+    int arr[]={5,4,3,2,1}; 
+
+    // Reverse part 
+    Reverse(arr,n); 
+    for(int i =0 ;i<n ; i++){
+        printf("%d\n",arr[i]);
+    printf("\n"); 
+    
+    }
+}
