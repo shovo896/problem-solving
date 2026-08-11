51c55edfa28cd2531539c8ed957db2be816f0a6a
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 21:28:48 +0600

    ckoijkl

--- dxhbhjk.c
+++ dxhbhjk.c
@@ -9,11 +9,6 @@ void swap(int *a, int *b){
 int main(){
     int arr[5] = {1, 2, 3, 4, 5};
 
-    for(int i = 0; i < 5; i++){
-        printf("%d\n", arr[i]);
-        printf("%d\n", *(arr + i));
-    }
-
     int n = 5;
     for(int i = 0; i < n / 2; i++){
         swap(&arr[i], &arr[n - i - 1]);
