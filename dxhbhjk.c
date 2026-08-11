b07226e39953d9167f12ff4893d4685311d9ea30
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-11 21:25:44 +0600

    uuy67gtiu

--- dxhbhjk.c
+++ dxhbhjk.c
@@ -1,40 +1,29 @@
-#include<stdio.h> 
+#include<stdio.h>
+
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 int main(){
+    int arr[5] = {1, 2, 3, 4, 5};
 
-    void swap(int *a , int *b){
-        int tmp = *a ;
-        *a = *b ;
-        *b = tmp ; 
-    }
-
-
-
-    int arr[5] = {1,2,3,4,5} ; 
-    for(int i=0 ;i<5 ; i++){
+    for(int i = 0; i < 5; i++){
         printf("%d\n", arr[i]);
-    printf("%d\n", *(arr+i));
+        printf("%d\n", *(arr + i));
     }
-    // reverse  part 
+
     int n = 5;
-    for(int i =0 ;i<n/2 ;i++){
-        int tmp=arr[i] ;
-        arr[i]=arr[n-i-1];
-        arr[n-i-1]=tmp; 
+    for(int i = 0; i < n / 2; i++){
+        swap(&arr[i], &arr[n - i - 1]);
     }
 
+    for(int i = 0; i < 5; i++){
+        printf("%d\n", arr[i]);
     }
-        
-    
-
-    for(int i = 0 ;i<5 ;i++){
-        printf("%d\n",arr[i]); 
 
     printf("\n");
-    }
 
-
-
-
-
-    return 0 ;
-    }
+    return 0;
+}
