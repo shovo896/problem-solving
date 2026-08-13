1d1a20e274b100ec3f8751630b30587ca7e9a631
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 22:40:27 +0600

    fklmvl;

--- string.cpp
+++ string.cpp
@@ -3,8 +3,9 @@
 using namespace std; 
 int main(){ 
     int arr[]={1,2,3,4,5};
-    sort(arr+2,arr+5);
-    for(int i=0;i<5;i++){
+    // sort(arr+2,arr+6);
+    reverse(arr+3,arr+5);
+    for(int i=0;i<6;i++){
         cout<<arr[i]<<" ";
     }
     return 0;
