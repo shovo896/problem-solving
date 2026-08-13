d76cbbd3cc9028860c19ce76f5fda7686d780964
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 23:02:33 +0600

    glkv

---
+++ jnsxz.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream> 
+using namespace std; 
+int main(){ 
+   // int a = 5 ; 
+    //cin >> a ; 
+    //cout << a << "\n";
+    //return 0;
+
+    // fast input output 
+
+    ios_base::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+    
+}
