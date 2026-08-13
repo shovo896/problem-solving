47eb859613a2d1d0d0577e319c89df4f6eacc61a
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 22:58:20 +0600

    fiockp

--- hjknk.cpp
+++ hjknk.cpp
@@ -6,12 +6,12 @@ int main(){
     int arr[]={1,2,3,4,5,6};
 
     // accumulate(start,end ,initial value)
-    int minNum=9999; 
-    for(int i=0 ;i<6;i++){
-        minNum=min(minNum,arr[i]); 
-        cout<<minNum<<"\n ";
-        cout <<*min_element(arr,arr+6)<<"\n";
-        for(int i=0;i<6;i++){
+   // int minNum=9999; 
+    //for(int i=0 ;i<6;i++){
+      //  minNum=min(minNum,arr[i]); 
+        //cout<<minNum<<"\n ";
+        //cout <<*min_element(arr,arr+6)<<"\n";
+       // for(int i=0;i<6;i++){
             cout<<arr[i]<<" ";
         }
         return 0;
