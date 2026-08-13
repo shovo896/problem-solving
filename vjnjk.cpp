360402dea1219ccf240dd9cb14c590354c10fe5f
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 22:44:25 +0600

    fhcbj

--- vjnjk.cpp
+++ vjnjk.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<algorithm> 
+#include <numeric>
 using namespace std; 
 int main(){
     int arr[]={1,2,3,4,5,6};
+
+    // accumulate(start,end ,initial value)
     cout<< accumulate(arr,arr+6,0)<<'\n';
     for(int i=0;i<6;i++){
         cout<<arr[i]<<" ";
