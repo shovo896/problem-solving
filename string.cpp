56089b3d2ff42c8e80bece430d103e6bd69ab2b4
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 22:37:55 +0600

    dxhc

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+using namespace std; 
+int main(){ 
+    int arr[]={1,2,3,4,5};
+    sort(arr+2,arr+5);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
