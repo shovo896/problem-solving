1d33973dc2ffe29e77ed0685bcf3323d1802bea8
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-13 22:16:54 +0600

    hhfd

--- xhjf.cpp
+++ xhjf.cpp
@@ -9,7 +9,12 @@ void SWAP(int *a, int *b) {
 int main(){
     int a =5 ,b= 10 ; 
     cout << "Before swapping: a = " << a << ", b = " << b << endl; 
-    SWAP(a,b);
+   //  SWAP(a,b);
+   SWAP(&a,&b);
     cout << "After swapping: a = " << a << ", b = " << b << endl; 
+
+    cout << "Maximum of a and b = " << max(a, b) << endl; 
+    cout << "Minimum of a and b = " << min(a, b) << endl; 
+
     return 0;
 }
